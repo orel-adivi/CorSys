39bb83965ffd6601db6cd0fa2e61ab1db1163760
Orel Adivi <57455071+orel-adivi@users.noreply.github.com>
2022-09-19 12:21:46 +0300

    Fourth commit

--- src/synthesizer/AstNodes.py
+++ src/synthesizer/AstNodes.py
@@ -1,2 +1,18 @@
+#
+#   @file : AstNodes.py
+#   @date : 19 September 2022
+#   @authors : Orel Adivi and Daniel Noor
+#
 import ast
 
+
+def generateIntLiteralNode(value: int, children: list, assignments: list):
+    node = ast.Constant(value=value)
+    node.results = map(lambda inp: value, assignments)
+    return node
+
+
+def generateVariableNode(name: str, children: list, assignments: list):
+    node = ast.Name(id=name, ctx=ast.Load())
+    node.results = map(lambda inp: inp[name], assignments)
+    return node
