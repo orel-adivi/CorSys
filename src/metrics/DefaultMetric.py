d507ab6195437710025890376da1b1d22cbd10d8
Orel Adivi <57455071+orel-adivi@users.noreply.github.com>
2022-09-23 14:54:54 +0300

    sixteenth commit

---
+++ src/metrics/DefaultMetric.py
@@ -0,0 +1,12 @@
+#
+#   @file : DefaultMetric.py
+#   @date : 23 September 2022
+#   @authors : Orel Adivi and Daniel Noor
+#
+from src.metrics.Metric import Metric
+
+
+class DefaultMetric(Metric):
+
+    def __init__(self):
+        super().__init__()
---
+++ src/metrics/Metric.py
@@ -0,0 +1,52 @@
+#
+#   @file : Metric.py
+#   @date : 23 September 2022
+#   @authors : Orel Adivi and Daniel Noor
+#
+from abc import ABC, abstractmethod
+
+
+class Metric(ABC):
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def intDistance(actual: int, expected: int) -> float:
+        if actual == expected:
+            return 0.0
+        return 1.0
+
+    @staticmethod
+    def floatDistance(actual: float, expected: float, EPS: float = 1e-3) -> float:
+        if abs(actual - expected) <= EPS:
+            return 0.0
+        return 1.0
+
+    @staticmethod
+    def strDistance(actual: str, expected: str) -> float:
+        if actual == expected:
+            return 0.0
+        return 1.0
+
+    def listDistance(self, actual: list, expected: list) -> float:
+        if len(actual) != len(expected):
+            return 1.0
+        dist = [self.distance(actual_element, expected_element)
+                for actual_element, expected_element in zip(actual, expected)]
+        sum_dist = sum(dist)
+        if sum_dist > 1.0:
+            return 1.0
+        return sum_dist
+
+    def distance(self, actual: object, expected: object) -> float:
+        if type(actual) == type(expected) == int:
+            return self.intDistance(actual=actual, expected=expected)
+        elif type(actual) == type(expected) == float:
+            return self.floatDistance(actual=actual, expected=expected)
+        elif type(actual) == type(expected) == str:
+            return self.strDistance(actual=actual, expected=expected)
+        elif type(actual) == type(expected) == list:
+            return self.listDistance(actual=actual, expected=expected)
+        return 1.0
