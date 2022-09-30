9cebf2cb2cee78ab363f73dc1beb096a4cf722a3
Orel Adivi <orel.adivi@cs.technion.ac.il>
2022-09-30 22:23:57 +0300

    update CI

--- RunAllTests.py
+++ RunAllTests.py
@@ -11,7 +11,7 @@ from tests.synthesizer.AstNodesTests import AstNodesTests
 class MyTestCase(AstNodesTests, unittest.TestCase):
 
     def testSanityUnittests(self):
-        self.assertEqual(True, True)
+        self.assertEqual(True, False)
 
 
 if __name__ == '__main__':
